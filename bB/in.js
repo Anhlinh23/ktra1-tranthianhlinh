49039560e6ea78fd980841d9746a481fd1d53759
Anhlinh23 <84840271+Anhlinh23@users.noreply.github.com>
2022-02-25 21:31:37 +0700

    Update in.js

--- bB/in.js
+++ bB/in.js
@@ -5,13 +5,9 @@ let d = 0;
 let a = Math.floor(Math.random() * 3);
 
 button1.addEventListener('click', function () {
-    // let arr = document.getElementsByTagName('input')
-    // console.log(arr);
-    // let number1 = arr[0].value;
     let  n = parseInt(number1);
     while (d < 3 && 0 <= n <= 10) {
         d++;
-       
         if (n === a && d <= 3) {
             alert("Bạn đã trúng thưởng")
             break;
